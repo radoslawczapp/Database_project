d01f79263dc2d3b24eaaf5fb4054b9efbb7c4169
radoslawczapp <czappradoslaw@gmail.com>
2019-01-21 20:23:23 +0100

    Header

--- includes/header.php
+++ includes/header.php
@@ -4,12 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login System</title>
-
-    <!-- Latest compiled and minified CSS -->
+    <title>Book Reviews</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <!-- Optional theme -->
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
 
     <style media="screen">
@@ -17,14 +13,6 @@
         margin-top: 70px;
       }
     </style>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
   </head>
   <body>
 
@@ -40,7 +28,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Login System</a>
+          <a class="navbar-brand" href="index.php">Book Reviews</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
